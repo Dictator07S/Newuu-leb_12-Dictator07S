a24516f8a34380f940e11d1bd609239e38a6fd11
ali <m.yoldoshev@newuu.uz>
2024-05-19 13:46:23 +0500

    leb

--- main.cpp
+++ main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
-    const int son = 10;
-    float *ptr, b[son], c;
-    float d = 0.3;
+int main()
+{
+    const int SIZE = 10;
+    float *ptr, arr[SIZE], overall_grade;
+    float midterm_weight = 0.3; // Midterm weights 30%
 
-    ptr = b;
-    for (int i = 0; i < son; i++)
+    // Read and store grades
+    cout << "Enter grades of 10 students for Midterm exam:\n";
+    ptr = arr;
+    for (int i = 0; i < SIZE; i++)
     {
         cin >> *ptr;
         ptr++;
     }
-    ptr = b;
-    for (int i = 0; i < son; i++){
-        c = *ptr * d;
-        cout << c << " ";
-        ptr++;
+
+    // Calculate overall grade and display in reverse order
+    ptr = arr + SIZE - 1; // Point to the last element of the array
+    for (int i = SIZE - 1; i >= 0; i--)
+    {
+        overall_grade = *ptr * midterm_weight;
+        cout << overall_grade << " ";
+        ptr--;
     }
-return 0;
+
+    return 0;
 }
